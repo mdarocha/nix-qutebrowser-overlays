2ed81bf329f876e5bd2280b6227cd422a12101b3
mdarocha <git@mdarocha.pl>
2022-10-06 20:12:13 +0200

    Add packages and fmt entries

--- flake.nix
+++ flake.nix
@@ -1,12 +1,25 @@
 {
   description = "qutebrowser with PR 4602 applied, adding support for tree-style tabs";
 
-  outputs = { self }: {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }: {
     overlays.default = final: prev: {
       qutebrowser-with-treetabs = prev.qutebrowser.overrideAttrs (old: {
         name = "qutebrowser-with-treetabs";
         patches = (old.patches or [ ]) ++ [ ./treetabs.patch ];
       });
     };
+
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+
+    packages.x86_64-linux =
+      let
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          overlays = [ self.overlays.default ];
+        };
+      in
+      { inherit (pkgs) qutebrowser-with-treetabs; };
   };
 }
