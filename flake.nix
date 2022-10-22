70bc737b85917bb8a585d1a68b776dae44a5b766
mdarocha <git@mdarocha.pl>
2022-10-22 15:38:16 +0200

    Update flakes, use existing qutebrowser

--- flake.nix
+++ flake.nix
@@ -1,12 +1,9 @@
 {
   description = "qutebrowser with PR 4602 applied, adding support for tree-style tabs";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-pyqt6.url = "github:LunNova/nixpkgs/lunnova/cura-upgrade";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, nixpkgs-pyqt6 }: {
+  outputs = { self, nixpkgs }: {
     overlays.default = final: prev: {
       qutebrowser-with-treetabs = prev.qutebrowser.overrideAttrs (old: rec {
         name = "${pname}-${old.version}";
@@ -15,27 +12,9 @@
         patches = (old.patches or [ ]) ++ [ ./treetabs.patch ];
       });
 
-      qutebrowser-qt-6 = prev.qutebrowser.overrideAttrs (old: let
-        pyqt6 = nixpkgs-pyqt6.legacyPackages.${prev.system}.python3.pkgs.pyqt6;
-      in rec {
-        name = "${pname}-${version}";
+      qutebrowser-qt-6 = prev.qutebrowser-qt6.overrideAttrs (old: rec {
+        name = "${pname}-${old.version}";
         pname = "qutebrowser-qt-6";
-        version = "unstable-06-10-22";
-
-        src = prev.runCommand "${name}-tarball"
-          {
-            src = prev.fetchFromGitHub {
-              owner = "qutebrowser";
-              repo = "qutebrowser";
-              rev = "5e11e6c7d413cf5c77056ba871a545aae1cfd66a";
-              sha256 = "sha256-5HNzPO07lUQe/Q3Nb4JiS9kb9GMQ5/FqM5029vLNNWo=";
-            };
-            binPath = prev.lib.makeBinPath [ prev.python3 ];
-            PYTHONPATH = "${pyqt6}";
-          } ''
-          export PATH="$binPath:$PATH"
-          python3 $src/scripts/dev/build_release.py --skip-packaging
-        '';
       });
     };
 
